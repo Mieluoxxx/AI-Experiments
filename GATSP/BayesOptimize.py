523a42d36f22f34939174ed4ca4dad714c70376c
moguw <weiyiding0@gmail.com>
2023-10-29 13:09:35 +0800

    add[10.29] GATSP annotation

--- GATSP/BayesOptimize.py
+++ GATSP/BayesOptimize.py
@@ -1,21 +1,33 @@
+"""
+    利用贝叶斯优化寻找最优参数
+"""
 from bayes_opt import BayesianOptimization
 from GATSP import TSP
 
-def _TSP(c_rate, m_rate, pop_size):     # 优化目标函数
+
+def _TSP(c_rate, m_rate, pop_size):
+    """
+    优化目标函数
+    :param c_rate:
+    :param m_rate:
+    :param pop_size:
+    :return:
+    """
     tsp = TSP(c_rate, m_rate, pop_size)
     return -tsp.best_dist
 
+
 if __name__ == "__main__":
-    iteration = 1200 # 迭代次数
-    seed = 2023     # 随机种子
+    iteration = 1200  # 迭代次数
+    seed = 2023  # 随机种子
 
     rf_bo = BayesianOptimization(
         _TSP,
-            {
-                'c_rate': (0.33,0.42),
-                'm_rate': (0.18,0.45),
-                'pop_size': (80,88),
-            }
+        {
+            "c_rate": (0.33, 0.42),
+            "m_rate": (0.18, 0.45),
+            "pop_size": (80, 88),
+        },
     )
 
     best_params = rf_bo.maximize()  # 获取最优参数组合
--- GATSP/pop-fitness.py
+++ GATSP/pop-fitness.py
@@ -1,12 +1,16 @@
-from GATSP import TSP
+"""
+    本文件用于绘制种群数量-种群适应度曲线
+"""
 from matplotlib import pyplot as plt
+
+from GATSP import TSP
 
 if __name__ == "__main__":
     c_rate = 0.3986  # 交叉阈值     0.4075 0.3986
     m_rate = 0.253  # 突变阈值    0.4345 0.253
-    iteration = 1200 # 迭代次数
-    seed = 2023     # 随机种子
-    pop_size = [20,30,40,50,60,70,80,90,100,110]
+    iteration = 1200  # 迭代次数
+    seed = 2023  # 随机种子
+    pop_size = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110]
     fitness = []
     epoch = [i for i in range(1, 11)]
 
@@ -16,7 +20,7 @@ if __name__ == "__main__":
 
     # 绘制种群数量-种群适应度曲线
     plt.plot(epoch, fitness)
-    plt.xlabel('pop_size')
-    plt.ylabel('fitness')
-    plt.title('pop_size-fitness')
-    plt.savefig('img/种群数量-种群适应度曲线.png')
+    plt.xlabel("pop_size")
+    plt.ylabel("fitness")
+    plt.title("pop_size-fitness")
+    plt.savefig("img/种群数量-种群适应度曲线.png")
